18238ae2eeb728b63cba81b8e69bbed9ad9091ba
Roger Fuentes <fuentesroger65@gmail.com>
2024-06-07 21:15:14 -0400

    feat: :sparkles: Adding energy

--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
+
 import random
 import simpy
-import time as tm
+import scipy.stats as st
 
 RANDOM_SEED = 42
 TASK_MEAN = 10.0
@@ -12,44 +13,74 @@ INTERRUPTION_SIGMA = 3.0
 BREAK_MEAN = 20.0
 BREAK_SIGMA = 3.0
 
-def time_per_task():
-    t = random.normalvariate(TASK_MEAN, TASK_SIGMA)
 
-    while t <= 0:
-        t = random.normalvariate(TASK_MEAN, TASK_SIGMA)
-    return t
+def time_per_task():
+    return st.expon.rvs(size=1, scale=TASK_MEAN)[0]
+
 
 def time_to_interrupt():
-    t = random.normalvariate(INTERRUPTION_MEAN, INTERRUPTION_SIGMA)
+    t = random.normalvariate(4, 1)
 
     while t <= 0:
-        t = random.normalvariate(INTERRUPTION_MEAN, INTERRUPTION_SIGMA)
+        t = random.normalvariate(4, 1)
     return t
 
-def time_to_break():
-    t = random.normalvariate(BREAK_MEAN, BREAK_SIGMA)
+def interrupt_duration():
+    return 3
+
+
+#Tiempo entre descansos
+#A -> descansos autorregulados
+#SL -> descansos sistematicos largos
+#SC -> descansos sistematicos cortos
+
+def A_time_to_break():
+    return st.expon.rvs(size=1, scale=BREAK_MEAN)[0]
+
+def SL_time_to_break():
+    return 24
+
+def SC_time_to_break():
+    return 12
+
+#Tiempo que toma el descanso, o duracion del descanso
+def A_break_duration():
+    t = random.normalvariate(14.67,13.71)
 
     while t <= 0:
-        t = random.normalvariate(BREAK_MEAN, BREAK_SIGMA)
+        t = random.normalvariate(14.67, 13.71)
     return t
 
-def break_duration():
-        time = random.normalvariate(BREAK_MEAN, BREAK_SIGMA)
+def SL_break_duration(time):
+    return 6
 
-        while time <= 0:
-            time = random.normalvariate(BREAK_MEAN, BREAK_SIGMA)
-        return time
+def SC_break_duration(time):
+    return 3
+
+
+def update_energy(person,energy):
+    person.energy += energy
+    if person.energy < 0 :
+        person.energy = 0
+        return False
+    else:
+        person.energy = min(person.energy,100)
+        return True
+
 
 
 
 class Person:
-    def __init__(self,env,state):
+    def __init__(self,env,state,time_to_break,break_duration):
         self.state = state
         self.env = env
         self.person = simpy.PreemptiveResource(env,capacity=1)
         self.completed_tasks = 0
         self.breaks = 0
         self.interrupts = 0
+        self.energy = 100
+        self.time_to_break = time_to_break
+        self.break_duration = break_duration
 
         self.process_working = env.process(self.working())
         env.process(self.interrupting())
@@ -66,6 +97,11 @@ class Person:
                     print('empieza a trabajar en el ', self.env.now, 'queda por trabajar ', time)
                     self.state = 'W'
                     yield self.env.timeout(time)
+                    print('se tiene una energia de ',self.energy)
+                    if not update_energy(self, -5 * time):             # update
+                        print('la energia ha llegado a 0')
+                        #detener simulacion
+                    print('la energia ahora es de ',self.energy)
                     time = 0
                     self.state = 'S'
                     print(f'tarea completada en el minuto ',self.env.now)
@@ -92,6 +128,9 @@ class Person:
                                 yield self.env.timeout(time_to_breakk)
                                 print('descanso completada en el minuto', self.env.now)
                                 self.breaks += 1
+                                print('se tiene una energia de ',self.energy)
+                                update_energy(self, time_to_breakk)             # update
+                                print('la energia ahora es de ',self.energy)
                                 time_to_breakk = 0
                                 #self.state = 'W'
                             except:
@@ -106,7 +145,7 @@ class Person:
                                 
                     
             self.completed_tasks +=1
-
+   
             print(f'tarea numero ',self.completed_tasks,' completada', self.env.now)
 
 
@@ -114,7 +153,7 @@ class Person:
     
     def take_break(self):
         while True:
-            time = time_to_break()
+            time = self.time_to_break()
             print('solicitando el proximo descanso en ',self.env.now, ' para dentro de ', time)
             yield self.env.timeout(time)
            
@@ -152,9 +191,12 @@ class Person:
 
 random.seed(RANDOM_SEED)
 
-env = simpy.Environment()
-person = Person(env,'S')
-env.run(until=200)
-print(person.breaks)
-print(person.completed_tasks)
-print(person.interrupts)
+#env = simpy.Environment()
+#person = Person(env,'S')
+#env.run(until=200)
+#print(person.breaks)
+#print(person.completed_tasks)
+#print(person.interrupts)
+
+for i in range(0,1000):
+    print(A_break_duration())
